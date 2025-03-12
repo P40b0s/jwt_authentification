278b334487c6b6ec929f6d0262d291b4029cab9a
P40b0s <uranius666@gmail.com>
2025-03-12 17:48:00 +0300

    add test and remove +60 to exp date

--- src/jwt.rs
+++ src/jwt.rs
@@ -57,20 +57,21 @@ impl JWT
             };
             jwt
     }
-
-    pub fn new_access<I: AsRef<str>, R>(&self, user_id: I, role: R) -> String
+    ///token lifetime in minutes
+    pub fn new_access<I: AsRef<str>, R>(&self, user_id: I, role: R, lifitime: i64) -> String
     where R: for<'de> Deserialize<'de> + Serialize + PartialEq + Clone
     {
         let iat =  OffsetDateTime::now_utc();
-        let exp = iat + Duration::minutes(5);
+        let exp = iat + Duration::minutes(lifitime);
         let claims = Claims { sub: user_id.as_ref().to_owned(),  exp, aud: None,  iat, role};
         encode(&jsonwebtoken::Header::new(self.algo.clone()), &claims, &self.encoding_key).unwrap()
     }
-    pub fn new_access_with_audience<I: AsRef<str>, R, A: ToString>(&self, user_id: I, role: R, audience: &[A]) -> String
+    ///token lifetime in minutes
+    pub fn new_access_with_audience<I: AsRef<str>, R, A: ToString>(&self, user_id: I, role: R, audience: &[A], lifitime: i64) -> String
     where R: for<'de> Deserialize<'de> + Serialize + PartialEq + Clone
     {
         let iat =  OffsetDateTime::now_utc();
-        let exp = iat + Duration::minutes(5);
+        let exp = iat + Duration::minutes(lifitime);
         let claims = Claims { sub: user_id.as_ref().to_owned(),  exp, aud: Some(audience.iter().map(|m| m.to_string()).collect()),  iat, role};
         encode(&jsonwebtoken::Header::new(self.algo.clone()), &claims, &self.encoding_key).unwrap()
     }
@@ -81,6 +82,8 @@ impl JWT
         let mut validation = Validation::new(self.algo.clone());
         validation.aud = None;
         validation.sub = Some(user_id.as_ref().to_owned());
+        //by default = 60 + 60 secs to key expired date
+        validation.leeway = 0;
         self.validate(token, validation)
     }
     pub fn validate_access_with_audience<R, I: AsRef<str>, A: ToString>(&self, token: I, user_id: I, audience: &[A]) -> Result<TokenData<Claims<R>>, AuthError>
@@ -89,6 +92,7 @@ impl JWT
         let mut validation = Validation::new(self.algo.clone());
         validation.sub = Some(user_id.as_ref().to_owned());
         validation.set_audience(audience);
+        validation.leeway = 0;
         self.validate(token, validation)
     }
 
@@ -120,6 +124,11 @@ impl JWT
                     logger::error!("Audience is invalid");
                     Err(AuthError::JWTValidateError("audience не совпадает с валидируемым".to_owned()))
                 },
+                ErrorKind::ExpiredSignature => 
+                {
+                    logger::error!("Token is expired");
+                    Err(AuthError::JWTValidateError("время жизни токена истекло".to_owned()))
+                }
                 _ => Err(AuthError::JWTError(err))
             },
         };
@@ -193,13 +202,28 @@ mod tests
         let role = "Operator";
         let aud = ["www.ya.ru", "www.yandex.ru"];
         let aud_check = ["www.yandex.ru"];
-        let key = jwt.new_access(&id, role.to_owned());
+        let key = jwt.new_access(&id, role.to_owned(), 5);
         logger::info!("access: {}",  key);
-        let upd = jwt.new_access(&id, role.to_owned());
+        let upd = jwt.new_access(&id, role.to_owned(), 5);
         let va = jwt.validate_access::<String, _>(&upd, &id).unwrap();
         logger::info!(" upd_access: {} user_id: {} role: {}", &upd, va.claims.user_id(), va.claims.role());
-        let upd2 = jwt.new_access_with_audience(&id, role.to_owned(), &aud);
+        let upd2 = jwt.new_access_with_audience(&id, role.to_owned(), &aud, 5);
         let claims: jsonwebtoken::TokenData<crate::Claims<String>> = jwt.validate_access_with_audience(&upd2, &id, &aud_check).unwrap();
         logger::info!("upd2_claims: {:?}", claims);
+    }
+    #[tokio::test]
+    async fn test_generated()
+    {
+        let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFZERTQSJ9.eyJzdWIiOiIxMjM0IiwiZXhwIjoxNzQxNzkwNjc2LCJpYXQiOjE3NDE3OTAzNzYsInJvbGUiOiJPcGVyYXRvciIsImF1ZCI6bnVsbH0.hKIYSkAYCyIKukBlbeMF6zvRFRuHsIZiKr-0XpTJXlzLHkTqta3hkA3Yp1NIMVAvey46zoCBw0Fn5S61naq2DQ";
+        let id = "1234";
+        let role = "Operator";
+        let _ = logger::StructLogger::new_default();
+        let mut jwt  = super::JWT::new_in_file("key.pkcs8");
+        let claims = jwt.validate_access::<String, _>(token, id);
+        logger::info!("claims: {:?}", claims);
+
+        //eyJ0eXAiOiJKV1QiLCJhbGciOiJFZERTQSJ9.eyJzdWIiOiIxMjM0IiwiZXhwIjoxNzQxNzkwNjc2LCJpYXQiOjE3NDE3OTAzNzYsInJvbGUiOiJPcGVyYXRvciIsImF1ZCI6bnVsbH0.hKIYSkAYCyIKukBlbeMF6zvRFRuHsIZiKr-0XpTJXlzLHkTqta3hkA3Yp1NIMVAvey46zoCBw0Fn5S61naq2DQ
+        //eyJ0eXAiOiJKV1QiLCJhbGciOiJFZERTQSJ9.eyJzdWIiOiIxMjM0IiwiZXhwIjoxNzQxNzkwNjc2LCJpYXQiOjE3NDE3OTAzNzYsInJvbGUiOiJPcGVyYXRvciIsImF1ZCI6bnVsbH0.hKIYSkAYCyIKukBlbeMF6zvRFRuHsIZiKr-0XpTJXlzLHkTqta3hkA3Yp1NIMVAvey46zoCBw0Fn5S61naq2DQ
+        //eyJ0eXAiOiJKV1QiLCJhbGciOiJFZERTQSJ9.eyJzdWIiOiIxMjM0IiwiZXhwIjoxNzQxNzkwNzc5LCJpYXQiOjE3NDE3OTA0NzksInJvbGUiOiJPcGVyYXRvciIsImF1ZCI6bnVsbH0.FZ7nN6ywCGRw-3n_Wu6IMnTAK9Crz73Nb22fSX7UvZM1UT_bjIGbtp2dlqbKzV7bmghV6L3wlT9iS8ec4WadDg
     }
 }
