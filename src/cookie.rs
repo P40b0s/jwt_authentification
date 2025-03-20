887bd3cdceafd3a67a056a44672264c51afc20af
P40b0s <uranius666@gmail.com>
2025-03-20 16:33:12 +0300

    export time::Duration

--- src/cookie.rs
+++ src/cookie.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 use cookie::Key;
-pub use cookie::{Cookie, CookieJar};
+pub use cookie::{Cookie, CookieJar, time::Duration};
 use ring::signature::Ed25519KeyPair;
 
 pub struct CookieService
--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,4 @@ mod validator;
 mod cookie;
 pub use error::JwtError;
 pub use jwt::{Claims, JWT, TokenData};
-pub use cookie::{CookieService, Cookie, CookieJar};
+pub use cookie::{CookieService, Cookie, CookieJar, Duration};
